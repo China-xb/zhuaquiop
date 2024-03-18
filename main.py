6a309668ea8e29416fa39503a11bbcde0911d5cb
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-18 22:10:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,39 @@
 import requests
-from python_telegram_bot import Bot
 
 def get_ips_from_url(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
             return response.text.splitlines()
+        else:
+            print(f"Failed to fetch IPs from {url}. Status code: {response.status_code}")
     except Exception as e:
         print(f"Error fetching IPs from {url}: {e}")
     return []
-
 def get_location(ip):
     try:
         response = requests.get(f"http://ip-api.com/json/{ip}")
         data = response.json()
         if data['status'] == 'success':
-            return data['countryCode']
+            # 更详细的地理位置描述
+            return f"{data['country']}"
     except Exception as e:
         print(f"Error fetching location for IP {ip}: {e}")
-    return None
-
-def convert_ips(input_urls, output_files, bot, chat_id):
+    return None 
+    
+def convert_ips(input_urls, output_files):
     for input_url, output_file in zip(input_urls, output_files):
-        ips = get_ips_from_url(input_url)
-
+        ips = get_ips_from_url(input_url)  # 获取URL中的IP地址列表
+        
         with open(output_file, 'w') as f:
             for ip in ips:
                 location = get_location(ip)
                 if location:
-                    f.write(f"{ip}:443#{location}n")
+                    f.write(f"{ip}:443#{location}\n")
                 else:
-                    f.write(f"{ip}#Unknownn")
-            bot.send_document(chat_id=chat_id, document=open(output_file, 'rb'))
+                    f.write(f"{ip}\n")
 
 if __name__ == "__main__":
-    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf",'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt','https://addressesapi.090227.xyz/CloudFlareYes']
+    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf",'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt','https://addressesapi.090227.xyz/CloudFlareYes']  # 包含IP地址的txt文件的多个URL
     output_files = ["bestproxy.txt", "bestcf.txt",'ip.txt','pure.txt']
-    bot_token = "6701932453:AAE2hVVH1WGvHeLxHX-m2laWqnKgz9FD4Dg"  # 替换为你的电报机器人令牌
-    chat_id = "1220184704"  # 替换为你的电报聊天ID
-    bot = Bot(token=bot_token)
-    convert_ips(input_urls, output_files, bot, chat_id)
+    convert_ips(input_urls, output_files)
