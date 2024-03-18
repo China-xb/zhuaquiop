97da790946788ed6e7b4eef269791f02b5381a46
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-18 20:42:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
 import requests
 
+# 添加telegram_bot_sendtext函数
+def telegram_bot_sendtext(bot_message, bot_token, bot_chatID):
+    
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+
+
 def get_ips_from_url(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
             return response.text.splitlines()
         else:
-            print(f"Failed to fetch IPs from {url}. Status code: {response.status_code}")
+            log_message = f"Failed to fetch IPs from {url}. Status code: {response.status_code}"
+            print(log_message)
+            telegram_bot_sendtext(log_message, bot_token, bot_chatID)
     except Exception as e:
-        print(f"Error fetching IPs from {url}: {e}")
+        log_message = f"Error fetching IPs from {url}: {e}"
+        print(log_message)
+        telegram_bot_sendtext(log_message, bot_token, bot_chatID)
     return []
+
+
 def get_location(ip):
     try:
         response = requests.get(f"http://ip-api.com/json/{ip}")
         data = response.json()
         if data['status'] == 'success':
-            # 更详细的地理位置描述
             return f"{data['country']}"
     except Exception as e:
-        print(f"Error fetching location for IP {ip}: {e}")
-    return None 
-    
-def convert_ips(input_urls, output_files):
+        log_message = f"Error fetching location for IP {ip}: {e}"
+        print(log_message)
+        telegram_bot_sendtext(log_message, bot_token, bot_chatID)
+    return None
+
+
+def convert_ips(input_urls, output_files, bot_token, bot_chatID):
     for input_url, output_file in zip(input_urls, output_files):
-        ips = get_ips_from_url(input_url)  # 获取URL中的IP地址列表
-        
+        ips = get_ips_from_url(input_url)
+
         with open(output_file, 'w') as f:
             for ip in ips:
                 location = get_location(ip)
                 if location:
-                    f.write(f"{ip}:443#{location}\n")
+                    f.write(f"{ip}:443#{location}n")
                 else:
-                    f.write(f"{ip}\n")
+                    f.write(f"{ip}n")
+        log_message = f"{output_file}文件更新完毕，已保存 {len(ips)} 条IP地址记录。"
+        print(log_message)
+        telegram_bot_sendtext(log_message, bot_token, bot_chatID)
 
 if __name__ == "__main__":
-    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf",'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt','https://addressesapi.090227.xyz/CloudFlareYes']  # 包含IP地址的txt文件的多个URL
+    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf",'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt','https://addressesapi.090227.xyz/CloudFlareYes']
     output_files = ["bestproxy.txt", "bestcf.txt",'ip.txt','pure.txt']
-    convert_ips(input_urls, output_files)
+    bot_token = '6701932453:AAE2hVVH1WGvHeLxHX-m2laWqnKgz9FD4Dg'
+    bot_chatID = '1220184704'
+    convert_ips(input_urls, output_files, bot_token, bot_chatID)
