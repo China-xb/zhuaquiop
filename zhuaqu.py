c2dec90f1c571dc441676733c92d9b6d4c44df0b
6693152 <137941170+6693152@users.noreply.github.com>
2024-03-18 12:39:39 +0800

    Update zhuaqu.py

--- zhuaqu.py
+++ zhuaqu.py
@@ -11,7 +11,7 @@ urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html',
 # 正则表达式用于匹配IP地址
 ip_pattern = r'd{1,3}.d{1,3}.d{1,3}.d{1,3}'
 
-# 检查ip.txt文件是否存在, 如果存在则删除它
+# 检查ip.txt文件是否存在,如果存在则删除它
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
@@ -20,6 +20,7 @@ with open('ip.txt', 'w') as file:
     for url in urls:
         # 发送HTTP请求获取网页内容
         response = requests.get(url)
+        response.encoding = 'utf-8'
         
         # 使用BeautifulSoup解析HTML
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -31,22 +32,14 @@ with open('ip.txt', 'w') as file:
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
-
+        
         # 遍历所有元素,查找IP地址
         for element in elements:
-            elementtext = element.gettext()
-            ipmatches = re.findall(ippattern, element_text)
+            element_text = element.get_text()
+            ip_matches = re.findall(ip_pattern, element_text)
             
-            # 如果找到IP地址
+            # 如果找到IP地址,在IP后面追加 ", 443"和将其位置写入文件
             for ip in ip_matches:
-                # 获取端口信息，此处假设端口信息与IP在同一元素中，且位于IP后，以':'分隔
-                portinfo = elementtext.split(ip + ':')-1
-                # 获取位置信息，此处假设位置信息与IP在同一元素中，且在字符串尾部
-                locationinfo = elementtext.split()-1
-
-                # 将IP, 端口和位置信息一并写入文件
-                file.write(ip + ':' + portinfo + ' ' + locationinfo + 'n')
+                file.write(ip + ',443#' + ip + 'n')
 
 print('IP地址已保存到ip.txt文件中。')
-
-我想强调的是，所有的假设（如端口和位置信息的位置及其与IP的关系）可能需要根据你要爬取的网站的实际内容进行调整。你应该检查这些网站，确定他们是如何在HTML中布局这些信息的，并且可能需要对BeautifulSoup的用法进行更深入的学习，以便更好地解析这些网页。
