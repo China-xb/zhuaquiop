975478c538744e606365d6d46b134909d96b1033
6693152 <137941170+6693152@users.noreply.github.com>
2024-03-18 14:00:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ def convert_ips(input_urls, output_files):
                     f.write(f"{ip}#Unknown\n")
 
 if __name__ == "__main__":
-    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf","https://cfno1.pages.dev/pure"]  # 包含IP地址的txt文件的多个URL
-    output_files = ["bestproxy.txt", "bestcf.txt", "bestcf.txt"]
+    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf"]  # 包含IP地址的txt文件的多个URL
+    output_files = ["bestproxy.txt", "bestcf.txt"]
     convert_ips(input_urls, output_files)
