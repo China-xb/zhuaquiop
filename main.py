2301ba80899c2793269e882add2c4c2873334589
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-18 21:14:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 
-# 添加telegram_bot_sendtext函数
-def telegram_bot_sendtext(bot_message, bot_token, bot_chatID):
-    
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+# 修改telegram_bot_sendtext函数为telegram_bot_senddocument
+def telegram_bot_senddocument(document, bot_token, bot_chatID):
 
-    response = requests.get(send_text)
+    send_document = 'https://api.telegram.org/bot' + bot_token + '/sendDocument'
+    files = {'document': open(document, 'rb')}
+    data = {'chat_id' : bot_chatID}
+    response = requests.post(send_document, files=files, data=data)
 
     return response.json()
-
 
 def get_ips_from_url(url):
     try:
@@ -50,10 +50,11 @@ def convert_ips(input_urls, output_files, bot_token, bot_chatID):
                     f.write(f"{ip}:443#{location}\n")
                 else:
                     f.write(f"{ip}\n")
-        log_message = f"{output_file}文件更新完毕，已保存 {len(ips)} 条IP地址记录。"
-        print(log_message)
-        telegram_bot_sendtext(log_message, bot_token, bot_chatID)
-
+...
+log_message = f"{output_file}文件更新完毕，已保存 {len(ips)} 条IP地址记录。"
+print(log_message)
+telegram_bot_senddocument(output_file, bot_token, bot_chatID)
+...
 if __name__ == "__main__":
     input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf",'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt','https://addressesapi.090227.xyz/CloudFlareYes']
     output_files = ["bestproxy.txt", "bestcf.txt",'ip.txt','pure.txt']
