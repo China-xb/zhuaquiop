8fd1c333af928cef24bcc40c2b62f999a9bb7ab8
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-19 03:14:40 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import socket
 
 def get_ips_from_url(url):
     try:
@@ -10,30 +11,46 @@ def get_ips_from_url(url):
     except Exception as e:
         print(f"Error fetching IPs from {url}: {e}")
     return []
+
 def get_location(ip):
     try:
         response = requests.get(f"http://ip-api.com/json/{ip}")
         data = response.json()
         if data['status'] == 'success':
-            # 更详细的地理位置描述
             return f"{data['country']}"
     except Exception as e:
         print(f"Error fetching location for IP {ip}: {e}")
-    return None 
-    
+    return None
+
+def scan_ports(ip):
+    for port in range(443, 6667): 
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            return port
+        sock.close()
+    return None
+
 def convert_ips(input_urls, output_files):
     for input_url, output_file in zip(input_urls, output_files):
-        ips = get_ips_from_url(input_url)  # 获取URL中的IP地址列表
+        ips = get_ips_from_url(input_url)  
         
         with open(output_file, 'w') as f:
             for ip in ips:
+                port = scan_ports(ip)
                 location = get_location(ip)
-                if location:
+                if port and location:
+                    f.write(f"{ip}:{port}#{location}\n")
+                elif location:
                     f.write(f"{ip}:443#{location}\n")
                 else:
                     f.write(f"{ip}\n")
 
 if __name__ == "__main__":
-    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf",'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt','https://addressesapi.090227.xyz/CloudFlareYes','https://kzip.pages.dev/kzip.txt?token=mimausb8']  # 包含IP地址的txt文件的多个URL
-    output_files = ["bestproxy.txt", "bestcf.txt",'ip.txt','pure.txt','kzip.txt']
+    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf",
+                  'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt',
+                  'https://addressesapi.090227.xyz/CloudFlareYes',
+                  'https://kzip.pages.dev/kzip.txt?token=mimausb8']
+    
+    output_files = ["bestproxy.txt", "bestcf.txt", 'ip.txt', 'pure.txt', 'kzip.txt']
     convert_ips(input_urls, output_files)
