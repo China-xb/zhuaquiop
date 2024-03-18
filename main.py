190adcd8bdc92ea7cad71d24d057056f34fa94ef
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-19 03:18:50 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,50 +1,34 @@
 import requests
-import socket
+import concurrent.futures
+from functools import lru_cache
 
-def get_ips_from_url(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.text.splitlines()
-        else:
-            print(f"Failed to fetch IPs from {url}. Status code: {response.status_code}")
-    except Exception as e:
-        print(f"Error fetching IPs from {url}: {e}")
-    return []
-
+@lru_cache(maxsize=None)
 def get_location(ip):
     try:
         response = requests.get(f"http://ip-api.com/json/{ip}")
         data = response.json()
         if data['status'] == 'success':
-            return f"{data['country']}"
+            return f"{data['country']} - {data['city']}"  # 更详细的地理位置描述
     except Exception as e:
         print(f"Error fetching location for IP {ip}: {e}")
     return None
 
-def scan_ports(ip):
-    for port in range(443, 6667): 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((ip, port))
-        if result == 0:
-            return port
-        sock.close()
-    return None
+def process_ip(ip):
+    location = get_location(ip)
+    if location:
+        return f"{ip}:443#{location}\n"
+    else:
+        return f"{ip}\n"
 
 def convert_ips(input_urls, output_files):
-    for input_url, output_file in zip(input_urls, output_files):
-        ips = get_ips_from_url(input_url)  
-        
-        with open(output_file, 'w') as f:
-            for ip in ips:
-                port = scan_ports(ip)
-                location = get_location(ip)
-                if port and location:
-                    f.write(f"{ip}:{port}#{location}\n")
-                elif location:
-                    f.write(f"{ip}:443#{location}\n")
-                else:
-                    f.write(f"{ip}\n")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for input_url, output_file in zip(input_urls, output_files):
+            ips = get_ips_from_url(input_url)
+
+            with open(output_file, 'w') as f:
+                results = list(executor.map(process_ip, ips))
+                for result in results:
+                    f.write(result)
 
 if __name__ == "__main__":
     input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf",
