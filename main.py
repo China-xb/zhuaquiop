cc1b6dc947b922bb127a26cba70ae4e5b27bae4f
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-20 00:13:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     updates = bot.get_updates(limit=3, timeout=10)
 
     all_ips_ports = []
-    for update in updates = await bot.get_updates()
+    for updates = await bot.get_updates() 
         ips_ports = extract_info_from_message(update.message.text)
         all_ips_ports.extend(ips_ports)
     write_ips_to_file(all_ips_ports, 'tg.txt')
