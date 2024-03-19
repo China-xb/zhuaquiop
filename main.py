a4e5ef824af5bfe18d66db37bbf9f8422e2f9009
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-19 18:09:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,14 +28,14 @@ def get_ips_from_url(url):
         print(f"Error fetching IPs from {url}: {e}")
     return []
 
-def get_region(ip):
+def get_location(ip):
     try:
         response = requests.get(f"http://ip-api.com/json/{ip}", timeout=3)  # 设置地区定位的超时为3秒
         data = response.json()
         if data['status'] == 'success':
             return data['regionName']
     except Exception as e:
-        print(f"Error fetching region for IP {ip}: {e}")
+        print(f"Error fetching location for IP {ip}: {e}")
     return "Unknown"
 
 def convert_ips(input_urls, output_files):
@@ -45,15 +45,18 @@ def convert_ips(input_urls, output_files):
 
             with open(output_file, 'w') as f:
                 for ip in ips:
-                    open_port = scan_ports(ip)
-                    region = get_region(ip)
-                    if open_port is not None:
-                        f.write(f"{ip}:{open_port}#{region}\n")
-                    else:
-                        if region == "Unknown":
-                            f.write(f"{ip}#No geolocation detected\n")
+                    try:
+                        region = get_location(ip)  # 获取IP地址的地区信息
+                        open_port = scan_ports(ip)  # 扫描IP地址的开放端口
+                        if open_port is not None:
+                            f.write(f"{ip}:{open_port}#{region}\n")  # 保存带有开放端口和地区信息的格式
                         else:
-                            f.write(f"{ip}#{region}\n")
+                            if region == "Unknown":
+                                f.write(f"{ip}#No geolocation detected\n")  # 保存未检测到地理位置的格式
+                            else:
+                                f.write(f"{ip}#{region}\n")  # 保存带有地区信息但未开放端口的格式
+                    except Exception as e:
+                        f.write(f"{ip}:443#Error occurred: {e}\n")  # 保存出现异常的IP地址及错误信息
 
 if __name__ == "__main__":
     input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf", 'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt', 'https://kzip.pages.dev/kzip.txt?token=mimausb8', 'https://addressesapi.090227.xyz/CloudFlareYes']  # 包含IP地址的txt文件的多个URL
