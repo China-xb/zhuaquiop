8919d0e3169dddbd862c9dc6bef17eba770b0974
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-20 01:09:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import socket
+import time
 
 def get_ips_from_url(url):
     try:
@@ -19,17 +21,17 @@ def get_location(ip):
             return data['countryCode']
     except Exception as e:
         print(f"Error fetching location for IP {ip}: {e}")
-    return None
+    return "Unknown"
 
-def verify_ports(ip, ports):
+def scan_ports(ip, ports):
     for port in ports:
-        try:
-            response = requests.get(f"http://{ip}:{port}")
-            if response.status_code in (200, 443, 8443, 2053, 2083, 2087, 2096):
-                return port
-        except Exception:
-            continue
-    return None
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)  # 设置超时时间为1秒
+        result = s.connect_ex((ip, port))
+        s.close()
+        if result == 0:
+            return port
+    return 443  # 如果所有端口都未开放，默认使用443端口
 
 def convert_ips(input_urls, output_files, ports):
     for input_url, output_file in zip(input_urls, output_files):
@@ -37,14 +39,15 @@ def convert_ips(input_urls, output_files, ports):
 
         with open(output_file, 'w') as f:
             for ip in ips:
-                location = get_location(ip.split(':')[0])  # 获取IP地址的位置，忽略端口（如果有）
-                port = ip.split(':')[-1] if ':' in ip else verify_ports(ip, ports)
-                if location and port:
-                    # IP地址已经包含端口，直接写入文件
-                    f.write(f"{ip}:{port}#{location}n")
-                elif location:
-                    # IP地址没有端口，但是位置已知，写入文件
-                    f.write(f"{ip}#Unknownn")
+                ip_parts = ip.split(':')
+                ip_address = ip_parts[0]
+                ip_port = int(ip_parts[1]) if len(ip_parts) > 1 else scan_ports(ip_address, ports)
+                
+                location = get_location(ip_address)
+                if location:
+                    f.write(f"{ip_address}:{ip_port}#{location}\n")
+                else:
+                    f.write(f"{ip_address}:{ip_port}#Unknown\n")
 
 if __name__ == "__main__":
     input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf", 'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt', 'https://addressesapi.090227.xyz/CloudFlareYes' , 'https://kzip.pages.dev/kzip.txt?token=mimausb8']
