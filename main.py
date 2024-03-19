651acaca57adc91b870341c3599da5afad72451a
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-19 16:29:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,6 @@ def convert_ips(input_urls, output_files):
                         f.write(f"{ip}#{location}\n")
 
 if __name__ == "__main__":
-    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf"]  # 包含IP地址的txt文件的多个URL
-    output_files = ["bestproxy.txt", "bestcf.txt"]
+    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf", 'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt']  # 包含IP地址的txt文件的多个URL
+    output_files = ["bestproxy.txt", "bestcf.txt"， 'ip.txt']
     convert_ips(input_urls, output_files)
