cede1e4e2d2f32033676387fd7227232ccb34f0b
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-23 00:22:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,8 @@ thread_pool = []
 for i in range(50):  # 根据需要调整线程数
     thread = threading.Thread(target=get_country_code, args=(ip_queue,))
     thread_pool.append(thread)
-    thread.start()
 
-def get_ips_from_url(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.text.splitlines()
-        else:
-            print(f"Failed to fetch IPs from {url}. Status code: {response.status_code}")
-    except Exception as e:
-        print(f"Error fetching IPs from {url}: {e}")
-    return []
-
+# 定义 get_country_code 函数
 def get_country_code(ip_queue):
     while True:
         try:
@@ -35,6 +24,7 @@ def get_country_code(ip_queue):
         except Exception as e:
             print(f"Error getting country code for IP {ip}: {e}")
 
+# 定义 get_location 函数
 def get_location(ip):
     try:
         response = requests.get(f"http://ip-api.com/json/{ip}")
@@ -45,6 +35,7 @@ def get_location(ip):
         print(f"Error fetching location for IP {ip}: {e}")
     return None
 
+# 定义扫描端口的函数
 def scan_ports(ip):
     open_ports = []
     for port in [8443, 2053, 2083, 2087, 2096]:
@@ -57,6 +48,7 @@ def scan_ports(ip):
         open_ports.append(443)
     return open_ports
 
+# 定义转换 IP 的函数
 def convert_ips(input_urls, output_files):
     for input_url, output_file in zip(input_urls, output_files):
         ips = get_ips_from_url(input_url)
@@ -79,3 +71,4 @@ if __name__ == "__main__":
     input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf", 'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt', 'https://addressesapi.090227.xyz/CloudFlareYes' , 'https://kzip.pages.dev/a.csv?token=mimausb8']  # 包含IP地址的txt文件的多个URL
     output_files = ["bestproxy.txt", "bestcf.txt", 'ip.txt', 'cfip.txt', 'kzip.txt']
     convert_ips(input_urls, output_files)
+
