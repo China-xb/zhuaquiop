c9335223311a7447c664bcbbb273c8a71e6f3f44
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-22 23:24:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import requests
-import socket
 import re
+import socket
+import requests
 
 def is_valid_ipv4(ip):
     """Check if the IP address is a valid IPv4 address."""
-    pattern = re.compile(r'^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))')
+    pattern = re.compile(r'^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
     return pattern.match(ip) is not None
 
 def get_ips_from_url(url):
+    """Extract IP addresses from a URL."""
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -30,6 +31,7 @@ def get_location(ip):
     return "火星star"
 
 def scan_ports(ip):
+    """Scan ports on an IP address."""
     open_ports = []
     try:
         for port in [8443, 2053, 2083, 2087]:
@@ -39,13 +41,14 @@ def scan_ports(ip):
             if result == 0:
                 open_ports.append(port)
                 break
-    except socket.gaierror as e:
+    except Exception as e:
         print(f"Error scanning ports for IP {ip}: {e}")
     if not open_ports:
         open_ports.append(443)
     return open_ports
 
 def convert_ips(input_urls, output_files):
+    """Convert a list of URLs to a list of IP addresses, location information, and scanned ports."""
     all_ips = []  # 存储所有 IP 地址
 
     # 获取所有 IP 地址
@@ -61,7 +64,7 @@ def convert_ips(input_urls, output_files):
             # 格式化保存信息
             info = f"{ip}:{open_ports[0]}#{location}\n"
         else:
-            info = f"{ip}:#\n"  # 对于不符合IPv4的地址，保存格式为IP:##
+            info = f"{ip}:#\n"  # 对于不符合IPv4的地址，保存格式为IP:#
 
         # 保存信息到对应的文件
         for output_file in output_files:
@@ -69,6 +72,12 @@ def convert_ips(input_urls, output_files):
                 f.write(info)
 
 if __name__ == "__main__":
-    input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf", 'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt', 'https://addressesapi.090227.xyz/CloudFlareYes' , 'https://kzip.pages.dev/a.csv?token=mimausb8']  # 包含IP地址的多个URL
+    input_urls = [
+        "https://ipdb.api.030101.xyz/?type=bestproxy",
+        "https://ipdb.api.030101.xyz/?type=bestcf",
+        'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt',
+        'https://addressesapi.090227.xyz/CloudFlareYes',
+        'https://kzip.pages.dev/a.csv?token=mimausb8'
+        ]
     output_files = ["bestproxy.txt", "bestcf.txt", 'ip.txt', 'cfip.txt', 'kzip.txt']
     convert_ips(input_urls, output_files)
